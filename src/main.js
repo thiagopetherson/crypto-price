f92b0fa1616fc5706dc18f48bb7815446cfb0d18
thiagopetherson <thiagopetherson@outlook.com>
2022-11-24 18:11:22 -0300

    Installing dependencies and configuring initial files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
+// Vue
 import { createApp } from 'vue'
 import App from './App.vue'
+// Vue Router
 import router from './router'
+// Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// Styles (Sass)
+import "./assets/sass/styles.sass"
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+let baseUrl = 'https://api.coingecko.com/api/v3'
+app.config.globalProperties.baseUrl = baseUrl
+
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
