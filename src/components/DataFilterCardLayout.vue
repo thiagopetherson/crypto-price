dc550f2a70ff6599419145f0ee6a54e09e82ffda
thiagopetherson <thiagopetherson@outlook.com>
2022-11-27 14:06:12 -0300

    Developing date component

---
+++ src/components/DataFilterCardLayout.vue
@@ -0,0 +1,40 @@
+<template>   
+  <div class="date-filter-card">
+    <h3>Your search...</h3>
+    <span class="date-filter-card-coin">{{ coin }}: <span>{{ coinValue }}</span></span>
+    <span class="date-filter-card-date">Date: <span>{{ date }}</span></span>
+    <span class="date-filter-card-time">Time: <span>{{ time }}</span></span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DateFilterCardLayout',
+  props: ['coin', 'date', 'time', 'coinValue'],  
+}
+
+</script>
+
+<style scoped lang="sass">
+.date-filter-card
+  width: 60%
+  display: flex
+  flex-direction: column
+  align-items: center 
+
+  h3
+    font-size: 2.4rem
+    font-weight: normal
+    
+  .date-filter-card-coin
+    text-transform: capitalize
+    font-size: 2rem
+    
+  .date-filter-card-date
+    font-size: 2rem
+
+  .date-filter-card-time
+    font-size: 2rem
+
+</style>
--- src/views/DateFilterView.vue
+++ src/views/DateFilterView.vue
@@ -1,34 +1,53 @@
 <template>
   <div class="date-filter">
-    <div class="date-filter-item-title">
-      <h1>{{ id }}</h1>
+    <div class="date-filter-title">
+      <h1>{{ coinId }}</h1>
       <h2>Choose the period</h2>
     </div>
-    <div class="date-filter-item-result">
-      <span class="date-filter-item-result-coin">{{ id }}: aa<span></span></span>
-      <span class="date-filter-item-result-date">Date: aa<span></span></span>
-      <span class="date-filter-item-result-time">Time: aa<span></span></span>
-    </div>  
-    <div class="date-filter-item-search">
-        <input class="date-filter-item-search-date" type="date" />
-        <input class="date-filter-item-search-time" type="time" />
-        <button class="date-filter-item-search-submit">Search</button>
+    <DataFilterCardLayout :coin="coinId" :date="form.date" :time="form.time" :coinValue="searchedCoinValue"  />
+    <div class="date-filter-search">
+        <input class="date-filter-search-date" type="date" v-model="form.date" />
+        <input class="date-filter-search-time" type="time" v-model="form.time" />
+        <button class="date-filter-search-submit" @click="getCoinByDate()">Search</button>
     </div>  
   </div>
 </template>
 
 <script>
+import DataFilterCardLayout from '@/components/DataFilterCardLayout.vue'
 
 export default {
   name: 'DateFilterView',
-  props: ['id'],
+  components: { DataFilterCardLayout },
+  props: ['coinId'],
   data () {
     return {
-      result: null,
+      form: {
+        date: "",
+        time: "",
+      },
+      searchedCoinValue: "",
+      showresult: false   
     }    
   },
+  methods: {
+    async getCoinByDate () {
+
+      let sdt = Math.floor(+new Date(`${this.form.date}T${this.form.time}`) / 1000) // Searched Date Timestamp   
+      let cdt = Math.floor(+new Date() / 1000) // Current Date Timestamps    
+      
+      await this.axios.get(`${this.baseUrl}/coins/${this.coinId}/market_chart/range?vs_currency=brl&from=${sdt}&to=${cdt}`)
+      .then(response => {
+        this.searchedCoinValue = response.data.prices[0][1]
+      })
+      .catch(error => {
+        console.log(error)
+      })      
+      
+    }
+  },
   mounted () {
-    console.log(this.id)
+    console.log(this.coinId)
   }
 }
 
@@ -39,48 +58,33 @@ export default {
 
 .date-filter
   width: 100%
-  height: 92vh
+  height: 60vh
   display: flex
   flex-direction: column
-
-  .date-filter-item-title
-    width: 100%
+  justify-content: space-around
+  align-items: center
+  
+  .date-filter-title
+    width: 40%
     display: flex
     flex-direction: column
-    align-items: center    
-   
-    h1
+    align-items: center
+    justify-content: center
+       
+    h1      
       font-size: $title-size
       font-weight: 400
       color: $secondary-color
       text-transform: uppercase
 
-    h2
+    h2      
       font-size: 2.4rem
-      font-weight: normal  
-  
-  .date-filter-item-result
-    width: 100%
-    display: flex
-    flex-direction: column
-    align-items: center    
-    
-    .date-filter-item-result-coin
-        text-transform: capitalize
-        font-size: 2rem
-    
-    .date-filter-item-result-date
-        font-size: 2rem
+      font-weight: normal
 
-    .date-filter-item-result-time
-        font-size: 2rem
-
-  .date-filter-item-search
-    width: 100%
+  .date-filter-search
+    width: 40%  
     display: flex
-    flex-direction: row
+    align-items: center
     justify-content: center
-
-
 
 </style>
